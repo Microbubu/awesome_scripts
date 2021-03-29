996b3d122421b1585dd6daa98ff8ec0a14afc1c1
Microbubu <microbubu@live.com>
2021-03-29 22:49:19 +0800

    issue: sort-object casesensitive not working

--- update_alidns.ps1
+++ update_alidns.ps1
@@ -24,8 +24,7 @@ function ComputeSignature {
         [string]$method,
         [hashtable]$paramDict
     )
-    
-    $combinedParams = ($paramDict.keys | Sort-Object | ForEach-Object { "$(UrlEncode $_)=$(UrlEncode $paramDict[$_])" }) -join "&"
+    $combinedParams = ($paramDict.Keys | Sort-Object -CaseSensitive | ForEach-Object { "$(UrlEncode $_)=$(UrlEncode $paramDict[$_])" }) -join "&"
     $stringToSign = $method.ToUpper() + "&" + (UrlEncode "/") + "&" + (UrlEncode $combinedParams)
     $hasher = [HMACSHA1]::new([Encoding]::UTF8.GetBytes($accessKeySecret + "&"))
     $hashedBytes = $hasher.ComputeHash([Encoding]::UTF8.GetBytes($stringToSign))
@@ -48,7 +47,6 @@ function BuildUrl {
         SignatureNonce = [random]::new().Next().ToString();
     }
     $paramDict.Keys | ForEach-Object { $queryDict.Add($_, $paramDict[$_]) }
-    $queryDict = GetRequstParamsDict $paramDict
     $queryDict.Signature = UrlEncode (ComputeSignature $method $queryDict)
     $aliUrlPrefix + (($queryDict.Keys | ForEach-Object { "$_=$($queryDict[$_])" }) -join "&")
 }
@@ -59,35 +57,42 @@ function FindDomain {
         DomainName = $domainName;
         PageNumber = 1
     };
-    $isFind 
+
     do {
         $url = BuildUrl "GET" $queryDict
-        $listDomainResponse = Invoke-WebRequest -Uri $url -Method "GET"
-        $dnsDomainNode = ([xml]$listDomainResponse.Content | Select-Xml -XPath "/DescribeDomainRecordsResponse/DomainRecords/Record[RR='$dnsRR']" | Select-Object -ExpandProperty node)
+        [xml]$xmlContent = (Invoke-WebRequest -Uri $url -Method "GET").Content
+        $dnsDomainNode = ($xmlContent | Select-Xml -XPath "/DescribeDomainRecordsResponse/DomainRecords/Record[RR='$dnsRR']" | Select-Object -ExpandProperty node)
         if ($null -ne $dnsDomainNode){
             $dnsDomainNode.RecordId
             $dnsDomainNode.Value
             return
         }
         else {
-            $queryDict.PageNumber += 1
+            if ($xmlContent.PageSize * $xmlContent.PageNumber -gt $xmlContent.TotalCount){
+                return
+            }
+            else { 
+                $queryDict.PageNumber += 1
+            }
         }
     } while (1 -eq 1)
 }
 
 
 $dnsDomain = FindDomain
-$recordId = $dnsDomain[1]
-$ipValue = $dnsDomain[2]
-$myIp = (Invoke-WebRequest -Uri "http://whatismyip.akamai.com").Content
-if ($ipValue -ne $myIp){
-    $queryDict = @{
-        Action = "UpdateDomainRecord";
-        RR = $dnsRR;
-        RecordId = $recordId;
-        Type = "A";
-        Value = $myIp
-    };
-    $url = BuildUrl "GET" $queryDict
-    Invoke-WebRequest -Uri $url -Method "GET"
+if ($null -ne $dnsDomain){
+    $recordId = $dnsDomain[0]
+    $ipValue = $dnsDomain[1]
+    $myIp = (Invoke-WebRequest -Uri "http://whatismyip.akamai.com").Content
+    if ($ipValue -ne $myIp){
+        $queryDict = @{
+            Action = "UpdateDomainRecord";
+            RR = $dnsRR;
+            RecordId = $recordId;
+            Type = "A";
+            Value = $myIp
+        };
+        $url = BuildUrl "GET" $queryDict
+        Invoke-WebRequest -Uri $url -Method "GET"
+    }
 }
